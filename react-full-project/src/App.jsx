69f7e8e2a8f90a5f820de45d1e4a10b122d023ac
Saad_Naseem <raosad786@gmail.com>
2025-06-19 23:58:08 +0500

    Initialize and Create login page

---
+++ react-full-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
+import Header from './components/others/Header'
+
+const App = () => {
+  return (
+    <div>
+  <EmployeeDashboard/>
+    </div>
+  )
+}
+
+export default App
