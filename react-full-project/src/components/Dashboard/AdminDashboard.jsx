af8d4bcd8110331042b14ae7ff9bc3426fb52bf5
Saad_Naseem <raosad786@gmail.com>
2025-06-20 19:04:15 +0500

    Implement authentication flow with admin, and employee dashboards

---
+++ react-full-project/src/components/Dashboard/AdminDashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../others/Header'
+import AllTask from '../others/AllTast'
+
+const AdminDashboard = () => {
+  return (
+    <div>
+      <Header/>
+
+<AllTask/>
+    </div>
+  )
+}
+
+export default AdminDashboard
