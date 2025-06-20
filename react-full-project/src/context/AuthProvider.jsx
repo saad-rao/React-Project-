011fec6448d57e0c250a6753b687de71cfca7cdf
Saad_Naseem <raosad786@gmail.com>
2025-06-20 20:50:19 +0500

    Add AuthProvider context for user authentication

---
+++ react-full-project/src/context/AuthProvider.jsx
@@ -0,0 +1,26 @@
+
+import React, { createContext ,useState, useEffect } from 'react'
+import { getLocalStorage, setLocalStorage } from '../utils/localStorage';
+
+export const AuthContext = createContext()
+
+
+export const AuthProvider = ({children}) => {
+
+  const [userData, setUserData] = useState(null);
+ useEffect(() => {
+        setLocalStorage()
+        const {employees} = getLocalStorage()
+        setUserData(employees)
+    }, [])
+
+  return (
+    <div>
+      <AuthContext.Provider value={[userData, setUserData]}>
+      {children}
+      </AuthContext.Provider>
+    </div>
+  )
+}
+    
+export default AuthProvider
