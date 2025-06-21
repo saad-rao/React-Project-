4fbc8498cd984fc6af60acabe955a21769e961c5
Saad_Naseem <raosad786@gmail.com>
2025-06-22 01:21:27 +0500

    Reorganize TaskList to render dynamic task components

--- react-full-project/src/components/TaskList/TaskList.jsx
+++ react-full-project/src/components/TaskList/TaskList.jsx
@@ -1,41 +1,29 @@
-import React from "react";
+import React from 'react'
+import AcceptTask from './AcceptTask'
+import NewTask from './NewTask'
+import CompleteTask from './CompleteTask'
+import FailedTask from './FailedTask'
 
-const TaskList = ({data}) => {
-  return (
-    <div
-      id="tasklist"
-      className="flex overflow-x-auto items-center justify-start gap-5 flex-nowrap h-[55%] w-full py-5 mt-10"
-    >
-      <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl">
-        <div className="flex justify-between items-center p-5">
-          <h3 className="bg-red-600 px-3 py-1 rounded text-sm">High</h3>
-          <h4 className="text">20 feb 2024</h4>
+const TaskList = ({ data }) => {
+    return (
+        <div id='tasklist' className='h-[50%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full py-1 mt-16'>
+            {data.tasks.map((elem, idx) => {
+                if (elem.active) {
+                    return <AcceptTask key={idx} data={elem} />
+                }
+                if (elem.newTask) {
+                    return <NewTask key={idx} data={elem} />
+                }
+                if (elem.completed) {
+                    return <CompleteTask key={idx} data={elem} />
+                }
+                if (elem.failed) {
+                    return <FailedTask key={idx} data={elem} />
+                }
+
+            })}
         </div>
-      </div>
+    )
+}
 
-      {/* <div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'>
-
-        </div>
-        <div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'>
-
-        </div>
-        <div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'>
-
-        </div>
-        <div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'>
-
-        </div>
-        <div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'>
-
-        </div>
-        <div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'>
-
-        </div>
-        <div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'>
-
-        </div> */}
-    </div>
-  );
-};
-
-export default TaskList;
+export default TaskList
