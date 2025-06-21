2d071f30782d57235af9fa28cc8e7e47a38514c0
Saad_Naseem <raosad786@gmail.com>
2025-06-22 01:14:06 +0500

    Refactor AuthProvider component structure
    Converted AuthProvider from a named to a default export and improved formatting. No functional changes were made; the code was reorganized for clarity and consistency.

--- react-full-project/src/context/AuthProvider.jsx
+++ react-full-project/src/context/AuthProvider.jsx
@@ -1,26 +1,28 @@
-
-import React, { createContext ,useState, useEffect } from 'react'
-import { getLocalStorage, setLocalStorage } from '../utils/localStorage';
+import React, { createContext, useEffect, useState } from 'react'
+import { getLocalStorage, setLocalStorage } from '../utils/localStorage'
 
 export const AuthContext = createContext()
 
+const AuthProvider = ({ children }) => {
+    // localStorage.clear()
 
-export const AuthProvider = ({children}) => {
+    const [userData, setUserData] = useState(null)
 
-  const [userData, setUserData] = useState(null);
- useEffect(() => {
+    useEffect(() => {
         setLocalStorage()
         const {employees} = getLocalStorage()
         setUserData(employees)
     }, [])
-
-  return (
-    <div>
-      <AuthContext.Provider value={[userData, setUserData]}>
-      {children}
-      </AuthContext.Provider>
-    </div>
-  )
-}
     
+    
+
+    return (
+        <div>
+            <AuthContext.Provider value={[userData,setUserData]}>
+                {children}
+            </AuthContext.Provider>
+        </div>
+    )
+}
+
 export default AuthProvider
